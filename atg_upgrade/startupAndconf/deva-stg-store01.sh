2d4770cc04c6798a9502e9a080688fbe3788cb63
ATG application user <atg@deva-stg-app01.alliancetime.com>
2017-09-21 03:02:58 +0000

    stg

--- atg_upgrade/startupAndconf/deva-stg-store01.sh
+++ atg_upgrade/startupAndconf/deva-stg-store01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export JBOSSCONF=deva-stg-store01
 rm -rf $JBOSS_HOME/standalone/$JBOSSCONF-tmp/*
-export RUN_CONF=$JBOSS_HOME/standalone/configuration/store.conf
+export RUN_CONF=$JBOSS_HOME/standalone/configuration/stg-store.conf
 $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0  -c $JBOSSCONF-config.xml -Djboss.server.temp.dir=$JBOSS_HOME/standalone/$JBOSSCONF-tmp
