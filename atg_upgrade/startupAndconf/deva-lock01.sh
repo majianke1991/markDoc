c3a19d91dff90bd0755497387a4bf124dc27f4f5
ATG application user <atg@deva-lock01.alliancetime.com>
2017-09-21 02:28:53 +0000

    xxxx

---
+++ atg_upgrade/startupAndconf/deva-lock01.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export JBOSSCONF=deva-lock01
+rm -rf $JBOSS_HOME/standalone/$JBOSSCONF-tmp/*
+export RUN_CONF=$JBOSS_HOME/standalone/configuration/lock.conf
+$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0  -c $JBOSSCONF-config.xml -Djboss.server.temp.dir=$JBOSS_HOME/standalone/$JBOSSCONF-tmp
